84571be524508643574a66d74502834a003bb6e3
sridhar <sridhar@vyaparapp.in>
2023-09-20 07:51:34 +0530

    ktor impl for android done.

--- androidApp/src/main/java/com/example/poc/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/poc/android/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.poc.Greeting
@@ -19,7 +26,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    var phrases by remember { mutableStateOf(listOf("Loading")) }
+                    LaunchedEffect(true) {
+                        phrases = try {
+                            Greeting().greet()
+                        } catch (e: Exception) {
+                            listOf(e.localizedMessage ?: "error")
+                        }
+                    }
+                    GreetingView(phrases)
                 }
             }
         }
@@ -27,14 +42,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun GreetingView(text: List<String>) {
+    Text(text = text.joinToString(separator = "\n"))
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        GreetingView(listOf("Hello, Android!", "asdasd"))
     }
 }
