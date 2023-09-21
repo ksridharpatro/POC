b09cab684cedae4b6700ae94f805741c517f5756
sridhar <sridhar@vyaparapp.in>
2023-09-21 07:35:18 +0530

    ios app fix

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,34 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = "Greeting().greet()"
+    @ObservedObject private(set) var viewModel: ViewModel
 
 	var body: some View {
-		Text(greet)
+        List(viewModel.phrases, id: \.self) { phrase in
+                    Text(phrase)
+                }
 	}
+}
+
+extension ContentView {
+    class ViewModel: ObservableObject {
+        @Published var phrases: [String] = ["Loading..."]
+        init() {
+            Greeting().greet { greeting, error in
+                        DispatchQueue.main.async {
+                            if let greeting = greeting {
+                                self.phrases = greeting
+                            } else {
+                                self.phrases = [error?.localizedDescription ?? "error"]
+                            }
+                        }
+                    }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+        ContentView(viewModel: ContentView.ViewModel())
 	}
 }
