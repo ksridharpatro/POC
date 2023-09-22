9c2028911f37f7bef7ccb55231191760e93c6af4
sridhar <sridhar@vyaparapp.in>
2023-09-22 08:37:31 +0530

    folder structure fix

--- androidApp/src/main/java/com/example/poc/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/poc/android/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.poc.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Text(text = "Hello World")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text(text = "Hello World")
+                }
             }
         }
     }
