02fa4728ea2a4dacd59e4b511caa7960c4b33f22
sridhar <sridhar@vyaparapp.in>
2023-10-02 13:24:22 +0530

    kmm poc shared code added.

---
+++ shared/src/commonMain/kotlin/com/example/poc/kmmpoc2/SpaceXSDK.kt
@@ -0,0 +1,24 @@
+package com.example.poc.kmmpoc2
+
+import RocketLaunch
+import com.example.poc.kmmpoc2.cache.Database
+import com.example.poc.kmmpoc2.cache.DatabaseDriverFactory
+import com.example.poc.kmmpoc2.network.SpaceXApi
+
+class SpaceXSDK(databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = Database(databaseDriverFactory)
+    private val api = SpaceXApi()
+
+    @Throws(Exception::class)
+    suspend fun getLaunches(forceReload: Boolean): List<RocketLaunch> {
+        val cachedLaunches = database.getAllLaunches()
+        return if (cachedLaunches.isNotEmpty() && !forceReload) {
+            cachedLaunches
+        } else {
+            api.getAllLaunches().also {
+                database.clearDatabase()
+                database.createLaunches(it)
+            }
+        }
+    }
+}
