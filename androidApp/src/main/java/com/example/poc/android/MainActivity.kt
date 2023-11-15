c7120e8d1ebb8695726a6584861e7681c37d518a
sridhar <sridhar@vyaparapp.in>
2023-11-15 18:41:08 +0530

    some poc related to local

--- androidApp/src/main/java/com/example/poc/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/poc/android/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.poc.android
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,9 +10,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.example.mylibrary.Test
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
+
+    override fun attachBaseContext(newBase: Context) {
+        val locale = Locale("")
+        Locale.setDefault(locale)
+        val config = Configuration(newBase.resources.configuration)
+        config.setLocale(locale)
+        super.attachBaseContext(newBase.createConfigurationContext(config))
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val test = Test()
@@ -21,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Text(text = "Hello World")
+                    Text(text = stringResource(id = R.string.local_example))
                 }
             }
         }
