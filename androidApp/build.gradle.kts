a97144f137e5c5cbddf850a4756d9926c7e955a0
sridhar <sridhar@vyaparapp.in>
2023-11-15 13:05:24 +0530

    some poc

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.poc.android"
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.example.poc.android"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
