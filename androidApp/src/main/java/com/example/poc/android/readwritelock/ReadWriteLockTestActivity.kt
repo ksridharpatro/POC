ff459f3d0f5f6fde4fe680570387aed2baf7b7c2
sridhar <sridhar@vyaparapp.in>
2024-04-30 14:43:42 +0530

    some poc

--- androidApp/src/main/java/com/example/poc/android/readwritelock/ReadWriteLockTestActivity.kt
+++ androidApp/src/main/java/com/example/poc/android/readwritelock/ReadWriteLockTestActivity.kt
@@ -6,13 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
 import com.example.poc.android.MyApplicationTheme
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class ReadWriteLockTestActivity : AppCompatActivity() {
-
-    val readWriteLock: ReadWriteLock = ReadWriteLock()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,22 +16,9 @@ class ReadWriteLockTestActivity : AppCompatActivity() {
                 Text(text = "Hello")
             }
         }
-
-        GlobalScope.launch {
-
-            readWriteLock.invokeUsingWriteLock {
-                Log.d("ReadWriteLockTest", "Write started")
-                delay(5000)
-                Log.d("ReadWriteLockTest", "Write completed")
-            }
-        }
-        GlobalScope.launch {
-            delay(2000)
-            readWriteLock.invokeUsingReadLock {
-                Log.d("ReadWriteLockTest", "Read started")
-                delay(5000)
-                Log.d("ReadWriteLockTest", "Read completed")
-            }
-        }
+        val filePath = applicationContext.getDatabasePath("dbFileName").path
+        val afilePath = applicationContext.getDatabasePath("dbFileName").absolutePath
+        Log.d("ReadWriteLockActivity", filePath)
+        Log.d("ReadWriteLockActivity", afilePath)
     }
 }
